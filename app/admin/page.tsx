66a65c192278d9268ac6982da65a843cfcba8fa3
Danrley Silva <danrleysil46@gmail.com>
2025-04-07 10:22:31 -0300

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,35 @@
+// pages/admin.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+import { db } from "@/app/lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+export default function Admin() {
+    const [stats, setStats] = useState({ sim: 0, nao: 0 });
+
+    useEffect(() => {
+        const fetchStats = async () => {
+            const snapshot = await getDocs(collection(db, "confirmacoes"));
+            let sim = 0, nao = 0;
+
+            snapshot.forEach(doc => {
+                const data = doc.data();
+                if (data.resposta === "sim") sim++;
+                else if (data.resposta === "nao") nao++;
+            });
+
+            setStats({ sim, nao });
+        };
+
+        fetchStats();
+    }, []);
+
+    return (
+        <div style={{ padding: "30px", fontFamily: "sans-serif" }}>
+            <h1>Confirmações</h1>
+            <p>✅ Sim: {stats.sim}</p>
+            <p>❌ Não: {stats.nao}</p>
+        </div>
+    );
+}
---
+++ app/convite/page.tsx
@@ -0,0 +1,84 @@
+// pages/convite.tsx
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import bg from "../public/convite-bg.jpeg";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../lib/firebase";
+
+export default function Convite() {
+    const searchParams = useSearchParams();
+    const nome = searchParams.get("nome");
+    const [respondido, setRespondido] = useState<boolean | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleResposta = async (resposta: string) => {
+        if (!nome) return;
+        setLoading(true);
+        await handleConfirmar(resposta);
+        if (resposta === "sim") {
+            setRespondido(true);
+        } else {
+            setRespondido(false);
+        }
+        setLoading(false);
+    };
+
+    const handleConfirmar = async (resposta: string) => {
+        if (!nome || !resposta) {
+            console.error("Dados inválidos");
+            return;
+        }
+
+        try {
+            const ref = doc(collection(db, "confirmacoes"), nome.toString());
+            await setDoc(ref, { resposta });
+            console.log("Resposta salva com sucesso");
+        } catch (err) {
+            console.error("Erro ao salvar resposta", err);
+        }
+    };
+
+    return (
+        <div className="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center px-4 text-center" style={{ backgroundImage: "url('/convite-bg.jpeg')" }}>
+            {respondido === true ? (
+                <h2 className="bg-white/80 text-xl md:text-2xl font-semibold px-6 py-4 rounded-2xl shadow-md text-black">
+                    Obrigado por confirmar, {nome}!
+                </h2>
+            ) : respondido === false ? (
+                <h2 className="bg-white/80 text-xl md:text-2xl font-semibold px-6 py-4 rounded-2xl shadow-md text-black">
+                    Tudo bem, entendemos {nome}!
+                </h2>
+            ) : respondido === null ? (
+                <div className="bg-white/85 max-w-md px-6 py-8 rounded-2xl shadow-lg">
+                    {loading ? (
+                        <div className="text-2xl font-bold mb-2 text-black">
+                            Enviando sua resposta...
+                        </div>
+                    ) : (
+                        <>
+                            <h1 className="text-2xl font-bold mb-2 text-black">Olá {nome},</h1>
+                            <p className="text-lg mb-6 text-black">Posso confirmar sua presença no Chá da Melinda?</p>
+                            <div className="flex gap-4 justify-center">
+                                <button
+                                    onClick={() => handleResposta("sim")}
+                                    className="bg-yellow-400 hover:bg-yellow-500 transition px-6 py-2 rounded-full font-semibold"
+                                >
+                                    Sim 🐝
+                                </button>
+                                <button
+                                    onClick={() => handleResposta("nao")}
+                                    className="bg-red-400 hover:bg-red-500 text-white transition px-6 py-2 rounded-full font-semibold"
+                                >
+                                    Não 😢
+                                </button>
+                            </div>
+                        </>
+                    )}
+                </div>
+            ) : null}
+        </div>
+    );
+}
